c60679af032d465fcb8ca2ed592af74969d323bc
dharaneeswarr <168128192+dharaneeswarr@users.noreply.github.com>
2025-04-06 20:19:30 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1:4d232a8a-a7d4-435f-89a9-40d21cd3231f', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7l57cac9bnagjtimi6vnrj0lo7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
